dcc27928babe12fc0958fefce23843d7d738aefa
pozyamb <pasha005pasha@gmail.com>
2025-05-13 13:20:35 +0300

    Delete yatube_api directory

--- yatube_api/api/mixins.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class CreateListRetrieveViewSet(mixins.CreateModelMixin,
-                                mixins.ListModelMixin,
-                                mixins.RetrieveModelMixin,
-                                viewsets.GenericViewSet):
-    pass
